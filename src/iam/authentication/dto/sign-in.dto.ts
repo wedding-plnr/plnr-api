fa6af167aeb593b5d54a4079296400cf4f967300
ace <kyleguerrero85@gmail.com>
2023-02-04 21:39:19 -0600

    chore: swagger notation added for auth DTOs

--- src/iam/authentication/dto/sign-in.dto.ts
+++ src/iam/authentication/dto/sign-in.dto.ts
@@ -1,9 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, MinLength } from 'class-validator';
 
 export class SignInDto {
   @IsEmail()
+  @ApiProperty()
   email: string;
 
   @MinLength(10)
+  @ApiProperty()
   password: string;
 }
--- src/iam/authentication/dto/sign-up.dto.ts
+++ src/iam/authentication/dto/sign-up.dto.ts
@@ -1,15 +1,20 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, Length, Max, MinLength } from 'class-validator';
 
 export class SignUpDto {
   @MinLength(5)
+  @ApiProperty()
   username: string;
 
   @Length(2, 2)
+  @ApiProperty()
   state: string;
 
   @IsEmail()
+  @ApiProperty()
   email: string;
 
   @MinLength(10)
+  @ApiProperty()
   password: string;
 }
